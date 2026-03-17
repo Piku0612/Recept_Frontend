8cf98b1af32461e8dc28ec3ecf200114f6b20612
diak <diak@mesterlevel.lo>
2026-03-17 11:44:39 +0100

    viteconfig

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
   server:{
     proxy:{
       '/users':{
-        target:'http://127.0.0.1:4000',
+        target:'http://192.168.10.110:4000',
         changeOrigin:true
       }
     }
